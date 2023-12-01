d98543658c981a84e3c2550ba50c294dcbbeb272
wighawag <wighawag@gmail.com>
2023-12-01 09:19:25 +0000

    ignore dist

--- eip-1193-in-memory/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import { EIP1193ProviderWithoutEvents } from 'eip-1193';
-import { Provider } from '@remix-project/remix-simulator';
-
-declare function wrapRemixSimulator(provider: Provider): EIP1193ProviderWithoutEvents;
-declare function createProvider(): EIP1193ProviderWithoutEvents;
-
-export { createProvider, wrapRemixSimulator };
